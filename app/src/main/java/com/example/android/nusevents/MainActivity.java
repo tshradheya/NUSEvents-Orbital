83489c43e641ffa24054920717cb6b25a671db23
Ronak Lakhotia <ronaklakhotia@Ronaks-MacBook-Pro.local>
2017-06-07 01:34:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/nusevents/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.nusevents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mToolBar = (Toolbar)findViewById(R.id.toolbar3);
+        setSupportActionBar(mToolBar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.my_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.setting )
+        {
+
+        }
+        return true;
+    }
+}
