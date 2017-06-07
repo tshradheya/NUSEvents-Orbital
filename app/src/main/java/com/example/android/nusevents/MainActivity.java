8c029e078bf6d07a003c3e04d7746ae4625a7298
Shradheya Thakre <tshradheya@gmail.com>
2017-06-07 08:54:53 +0530

    Changed interface. Floating button added. Few other changes

--- app/src/main/java/com/example/android/nusevents/MainActivity.java
+++ app/src/main/java/com/example/android/nusevents/MainActivity.java
@@ -14,8 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar mToolBar = (Toolbar)findViewById(R.id.toolbar3);
-        setSupportActionBar(mToolBar);
+
 
     }
 
