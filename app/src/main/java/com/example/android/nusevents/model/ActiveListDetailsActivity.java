036265d03bc8da26417b77b66d3efc9a391d0994
Shradheya Thakre <tshradheya@gmail.com>
2017-07-03 23:13:26 +0530

    Update Feature works perfectly

--- app/src/main/java/com/example/android/nusevents/model/ActiveListDetailsActivity.java
+++ app/src/main/java/com/example/android/nusevents/model/ActiveListDetailsActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.nusevents.model;
 
 import android.app.AlertDialog;
+import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,9 +15,13 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.nusevents.DatePickerFragment;
+import com.example.android.nusevents.DatePickerUpdateFragment;
 import com.example.android.nusevents.DisplayEventList;
 import com.example.android.nusevents.MainActivity;
 import com.example.android.nusevents.R;
+import com.example.android.nusevents.TimePickerFragment;
+import com.example.android.nusevents.TimePickerUpdateFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -23,30 +29,52 @@ import com.google.firebase.database.FirebaseDatabase;
 
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class ActiveListDetailsActivity extends AppCompatActivity {
+import static com.example.android.nusevents.Details.dateButton;
+import static com.example.android.nusevents.Details.timeButton;
+
+public class ActiveListDetailsActivity extends FragmentActivity {
 
     private ListView mListView;
     private TextView mTextViewListName, mTextViewListOwner;
     private TextView mTextViewInfo,getmTextViewTime,getmTextViewLocation;
 
+    public static Button dateUpdate;
+    public static Button timeUpdate;
+
+
+    public static int year,month, day, hour,min;
+
+
+    View dialogview;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_list);
 
+        //dateUpdate=(Button)dialogview.findViewById(R.id.datepickerUpdate);
+        //timeUpdate =(Button)dialogview.findViewById(R.id.timepickerUpdate);
+
         mListView = (ListView) findViewById(R.id.main_list);
-        mTextViewListName = (TextView) findViewById(R.id.about_time);
+        //mTextViewListName = (TextView) findViewById(R.id.about);
         mTextViewListOwner = (TextView) findViewById(R.id.organize_by);
         mTextViewInfo = (TextView) findViewById(R.id.about_the_event);
         getmTextViewTime = (TextView) findViewById(R.id.about_time);
         getmTextViewLocation = (TextView) findViewById(R.id.about_loc_event);
 
 
-        String dAndT="";;
+        String dAndT="";
         Intent i =getIntent();
         final  String id = i.getStringExtra(DisplayEventList.event_id);
         final  String name = i.getStringExtra(DisplayEventList.event_name);
@@ -114,9 +142,12 @@ public class ActiveListDetailsActivity extends AppCompatActivity {
     private void showUpdateDialog(final String eventid,  final String userid)
 
     {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
         LayoutInflater layoutInflater = getLayoutInflater();
-        final View dialogview = layoutInflater.inflate(R.layout.update,null);
+
+
+        dialogview = layoutInflater.inflate(R.layout.update,null);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
 
         dialog.setView(dialogview);
 
@@ -126,6 +157,15 @@ public class ActiveListDetailsActivity extends AppCompatActivity {
         final EditText editTextinfo = (EditText)dialogview.findViewById(R.id.about_eventUpdate);
         final EditText editTextloc = (EditText)dialogview.findViewById(R.id.locationUpdate);
         final EditText editTextown = (EditText)dialogview.findViewById(R.id.organizeUpdate);
+
+        dateUpdate=(Button)dialogview.findViewById(R.id.datepickerUpdate);
+        timeUpdate =(Button)dialogview.findViewById(R.id.timepickerUpdate);
+
+
+
+
+
+
 
         //time and date to be done
 
@@ -143,7 +183,23 @@ public class ActiveListDetailsActivity extends AppCompatActivity {
                 String newown = editTextown.getText().toString().trim();
                 String newinfo = editTextinfo.getText().toString().trim();
 
-                updatedetails(eventid,newname,newloc,newown,newinfo,1234567,userid);
+                String dateString=day+"/"+month+"/"+year+" "+hour+":"+min;
+
+                long eventDateLong=0;
+
+                try {
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
+                    Date eventDate = sdf.parse(dateString);
+                    eventDateLong=eventDate.getTime();
+
+                }
+
+                catch (ParseException e){
+
+                }
+
+                updatedetails(eventid,newname,newloc,newown,newinfo,eventDateLong,userid);
                 alertDialog.dismiss();
 
             }
@@ -160,4 +216,52 @@ public class ActiveListDetailsActivity extends AppCompatActivity {
         Toast.makeText(this,"Event Updated Successfully!",Toast.LENGTH_LONG).show();
         return true;
     }
+
+
+    public void showDateDialog(View v) {
+        DialogFragment newFragment = new DatePickerUpdateFragment();
+
+        newFragment.show(getFragmentManager(),"datePicker");
+
+
+    }
+
+    public void showTimeDialog(View v) {
+        DialogFragment newFragment = new TimePickerUpdateFragment();
+        newFragment.show(getFragmentManager(), "TimePicker");
+    }
+
+
+
+
+    public static void showUpdateDate(int y,int m,int d)
+    {
+        year=y;
+        month=m+1;
+        day=d;
+
+
+        dateUpdate.setText(d+"-"+month+"-"+y);
+    }
+
+
+    public static void showUpdateTime(int h,int m)
+    {
+
+        hour=h;
+        min=m;
+
+        if(m/10==0) {
+
+
+            timeUpdate.setText(h + ":0" + m);
+        }
+
+        else{
+            timeUpdate.setText(h + ":" + m);
+
+        }
+    }
+
+
 }
